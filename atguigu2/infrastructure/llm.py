498ba33d8f85786149b8ba003f457c78198b0a4d
Neptune668 <2935971352@qq.com>
2026-08-05 21:04:14 +0800

    llm

---
+++ atguigu2/infrastructure/llm.py
@@ -0,0 +1,16 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.language_models import BaseChatModel
+
+from atguigu.conf.config import settings
+
+llm: BaseChatModel = init_chat_model(
+    model = settings.llm_model,
+    model_provider="openai",
+    base_url=settings.llm_base_url,
+    api_key=settings.llm_api_key,
+    temperature=0
+)
+
+if __name__ == '__main__':
+    print(llm.invoke("你好").content)
+
